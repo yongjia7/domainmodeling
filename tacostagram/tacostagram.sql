c2c09c0da6abd7c40be32a86d32d851797b545e9
yongjia7 <yongjia.zhang@kellogg.northwestern.edu>
2022-01-20 03:04:27 +0000

    hard

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,33 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT,
+  real_name TEXT,
+  location TEXT
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  image_file TEXT,
+  posted_at TEXT
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER,
+  body TEXT
+);
+
+CREATE TABLE followers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  followed_user_id INTEGER,
+  follower_user_id INTEGER
+);
 
