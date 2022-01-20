d5163ee71b7f4da3240b6d637f85621474f33a3c
yongjia7 <yongjia.zhang@kellogg.northwestern.edu>
2022-01-20 02:15:41 +0000

    trying

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,43 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  title TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  created_at TEXT,
+  notes TEXT,
+  salesperson_id INTEGER,
+  contact_id INTEGER
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE industry_memberships (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  industry_id INTEGER
+);
